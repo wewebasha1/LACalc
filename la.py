8bc77961d9c61198eb34ebbfa1d3bd651ee4fb13
Matthew Martin <mattmartin2004@gmail.com>
2025-09-02 22:20:44 -0400

    error handling for making arrays

--- la.py
+++ la.py
@@ -1,23 +1,27 @@
 import sys
 from matrix import Matrix
 from fractions import Fraction
+
+
 def main():
-    while(True):
+    matrix = None
+    while True:
         command = input(":")
-        response = menu(command)
+        response = command_processor(command, matrix)
         print(response[1])
 
-def menu(command):
-    global matrix
+
+def command_processor(command, matrix):
     if command == "quit":
         sys.exit()
     commands = command.split(" ")
     match commands[0]:
         case "make":
-            matrix = make_array(commands[1], commands[2], commands[3])
-            return matrix, "matrix created"
+            response = make_array(commands[1], commands[2], commands[3])
+            return response[0], response[1]
         case "addr":
-            pass
+            response = addr(commands[1], commands[2])
+            return response[0], response[1]
         case "addc":
             pass
         case "replace":
@@ -31,37 +35,52 @@ def menu(command):
             return matrix, ""
         case _:
             return matrix, "command not recognized"
-def make_array(dimensions, variable, nums):
-    global variables
 
+
+def make_array(dimensions, variable, values):
+    # save dimensions
     dimensions_list = dimensions.split('x')
+    if len(dimensions_list) != 2:
+        return None, "incorrect dimensions, matrix not created"
+    if not dimensions_list[0].isdigit or not dimensions_list[1].isdigit:
+        return None, "incorrect dimensions, matrix not created"
     rows = int(dimensions_list[0])
     columns = int(dimensions_list[1])
-
-    # make coefficient matrix
-    row_list = nums.split(':')
-
-    coefficient_matrix = []
-    for row in range(rows):
-        row_str_list = row_list[row].split(';')
-        row_num_list = list(map(float, row_str_list))
-        coefficient_matrix.append(row_num_list)
+    if rows <= 0 or columns <= 0:
+        return None, "incorrect dimensions, matrix not created"
 
     # make variables
-    SUB = str.maketrans("0123456789", "₀₁₂₃₄₅₆₇₈₉")
+    variables = None
+    sub = str.maketrans("0123456789", "₀₁₂₃₄₅₆₇₈₉")
     for index in range(columns):
         variables = variable + str(index) + ' '
-    variables.translate(SUB)
+    variables.translate(sub)
     variables.strip()
+
+    # make coefficient matrix
+    try:
+        row_list = values.split(':')
+        coefficient_matrix = []
+        for row in range(rows):
+            row_str_list = row_list[row].split(',')
+            row_num_list = list(map(float, row_str_list))
+            coefficient_matrix.append(row_num_list)
+    except ValueError:
+        return None, "incorrect values, matrix not created"
+    except IndexError:
+        return None, "incorrect values, matrix not created"
 
     # return matrix
     matrix = Matrix(rows, columns, coefficient_matrix, variables)
-    return matrix
+    return matrix, "matrix created"
 
-def addr():
-    row_str_list = input(':').split(' ')
+
+def addr(matrix, row):
+    row_str_list = row.split(',')
     row_int_list = list(map(int, row_str_list))
     matrix.add_row(row_int_list)
+    return matrix, "row added"
+
 
 if __name__ == '__main__':
     main()
--- test_la.py
+++ test_la.py
@@ -1,25 +1,81 @@
-import sys
-import pytest
 import la
-import matrix
 
-def test_make():
-    matrix_1 = ("1;2;3:"
-              "1;2;3:"
-              "1;2;3:")
-    assert la.menu(f"make 3x3 x {matrix_1}") == "matrix created"
-    matrix_2 = ("0;-72;3:"
-                "-01;3.2;-0.0:"
-                "5.21;0;2:"
-                "-20000;5;8:")
-    assert la.menu(f"make 4x3 x {matrix_2}") == "matrix created"
-    matrix_3 = ("0;-72;3;0;0;12;7;-61000")
-    assert la.menu(f"make 1x8 x {matrix_3}") == "matrix created"
 
-def test_make_error():
-    pass
+def test_make_dimensions():
+    matrix_1 = ("1,2,3:"
+                "1,2,3:"
+                "1,2,3:")
+    assert la.command_processor(f"make 0x-1 _ {matrix_1}", None)[1] == "incorrect dimensions, matrix not created"
+    matrix_2 = ("1,2,3:"
+                "1,2,3:"
+                "1,2,3:")
+    assert la.command_processor(f"make 6x0 _ {matrix_2}", None)[1] == "incorrect dimensions, matrix not created"
+    matrix_3 = ("1,2,3:"
+                "1,2,3:"
+                "1,2,3:")
+    assert la.command_processor(f"make 0-1 _ {matrix_3}", None)[1] == "incorrect dimensions, matrix not created"
+    matrix_4 = ("1,2,3:"
+                "1,2,3:"
+                "1,2,3:")
+    assert la.command_processor(f"make 33 _ {matrix_4}", None)[1] == "incorrect dimensions, matrix not created"
+    matrix_5 = ("1,2,3:"
+                "1,2,3:"
+                "1,2,3:")
+    assert la.command_processor(f"make xxx _ {matrix_5}", None)[1] == "incorrect dimensions, matrix not created"
+    matrix_5 = ("1,2,3:"
+                "1,2,3:"
+                "1,2,3:")
+    assert la.command_processor(f"make 6x6x6x6x6x6x6 _ {matrix_5}", None)[1] == ("incorrect dimensions, matrix not "
+                                                                                 "created")
 
-def test_print():
-    matrix_3 = ("0;-72;3;0;0;12;7;-61000")
-    assert la.menu(f"make 1x8 x {matrix_3}") == "matrix created"
-    assert la.menu("print") == ""
+
+def test_make_variable():
+    matrix_1 = ("1,2,3:"
+                "1,2,3:"
+                "1,2,3:")
+    assert la.command_processor(f"make 3x3 ear {matrix_1}", None)[1] == "matrix created"
+    matrix_1 = ("1,2,3:"
+                "1,2,3:"
+                "1,2,3:")
+    assert la.command_processor(f"make 3x3 3 {matrix_1}", None)[1] == "matrix created"
+
+
+def test_make_values():
+    matrix_1 = ("1,2,3:"
+                "1,2,3:"
+                "1,2,3:")
+    assert la.command_processor(f"make 3x3 x {matrix_1}", None)[1] == "matrix created"
+    matrix_2 = ("0,-72,3:"
+                "-01,3.2,-0.0:"
+                "5.21,0,2:"
+                "-20000,5,8:")
+    assert la.command_processor(f"make 4x3 x {matrix_2}", None)[1] == "matrix created"
+    matrix_3 = "0,-72,3,0,0,12,7,-61000"
+    assert la.command_processor(f"make 1x8 x {matrix_3}", None)[1] == "matrix created"
+    matrix_4 = (",2,3:"
+                "1,3:"
+                "1,2,3:")
+    assert la.command_processor(f"make 3x3 x {matrix_4}", None)[1] == "incorrect values, matrix not created"
+    matrix_5 = "34"
+    assert la.command_processor(f"make 3x3 x {matrix_5}", None)[1] == "incorrect values, matrix not created"
+    matrix_6 = ','
+    assert la.command_processor(f"make 3x3 x {matrix_6}", None)[1] == "incorrect values, matrix not created"
+
+
+def test_addr_matrix():
+    coefficient_matrix_1 = ("1,2,3:"
+                            "1,2,3:"
+                            "1,2,3:")
+    matrix_1 = la.command_processor(f"make 3x3 x {coefficient_matrix_1}", None)[0]
+    assert la.addr(matrix_1, "1,2,3")[1] == "row added"
+    coefficient_matrix_2 = ("0,-72,3:"
+                            "-01,3.2,-0.0:"
+                            "5.21,0,2:"
+                            "-20000,5,8:")
+    matrix_2 = la.command_processor(f"make 4x3 x {coefficient_matrix_2}", None)[0]
+    assert la.addr(matrix_2, "1,2,3")[1] == "row added"
+    coefficient_matrix_3 = "0,-72,3,0,0,12,7,-61000"
+    matrix_3 = la.command_processor(f"make 1x8 x {coefficient_matrix_3}", None)[0]
+    assert la.addr(matrix_3, "1,2,3,4,5,6,7,8")[1] == "row added"
+
+    # add commands with incorrect parameters for addr
